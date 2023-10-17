5f086f026c5cc83d4d26f210c9f3b15c3bfe2ac2
MarsImmortal <s222198007@deakin.edu.au>
2023-10-18 01:22:41 +1100

    connect mongodb with api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { env } from 'process';
+
+dotenv.config();
+
+mongoose.connect(process.env.URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch((e) => console.log(e));
+
 
 const app = express();
 const PORT = 3001;
