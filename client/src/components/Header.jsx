86cd847231e917035cbe491cc321978e25bc6cda
MarsImmortal <s222198007@deakin.edu.au>
2023-10-23 15:18:55 +1100

    modify and personalise the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+    const {currentUser} = useSelector(state => state.user )
   return (
     <header className='bg-gradient-to-t from-white to-lime-400 '>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -17,14 +19,20 @@ const Header = () => {
                 </h1>
             </Link>
             <ul className='flex gap-4'>
-                <Link to='/'>
-                <li className='hidden sm:inline text-black hover:text-green-500'> Home </li>
+                <Link 
+                to='/'>
+                    <li className='hidden sm:inline text-black hover:text-green-500'> Home </li>
                 </Link>
-                <Link to='/About'>
-                <li className=' hidden sm:inline text-black hover:text-green-500'> About </li>
+                <Link 
+                to='/About'>
+                    <li className=' hidden sm:inline text-black hover:text-green-500'> About </li>
                 </Link>
-                <Link to='/sign-in'>
-                <li className=' text-black hover:text-green-500'> Sign in </li>
+                <Link 
+                to='/profile'>
+                    {currentUser ? (
+                        <img className='rounded-full h-7 w-7 object-cover' src= {currentUser.avatar} alt='profile' />
+                    ): < li className=' text-black hover:text-green-500'> Sign in </li>
+                    } 
                 </Link>
             </ul>
             <form className='bg-gradient-to-t from-white to-lime-400 p-3 rounded-lg flex items-center '>
